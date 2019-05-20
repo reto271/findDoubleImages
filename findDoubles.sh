6c37dc95c976047abeccfb70f9e50db8d894a33b
Reto <reto271@gmx.ch>
2019-05-20 20:56:28 +0200

    Improve performance
    Do no longer use verbose. Print only size of the image and use the file size.

--- findDoubles.sh
+++ findDoubles.sh
@@ -43,32 +43,10 @@ while read FILE ; do
     ACTUAL_FILE_SIZE=$(du -b "${FILE}" | cut -f 1)
     #echo "ACTUAL_FILE_SIZE: ${ACTUAL_FILE_SIZE}"
 
-    # exif:Model: NIKON D5100
-    # signature: 9d473e2c68ab78cc718deb2a06d7fae31d83e09fae812ef8c760ac012d701694
-    # Geometry: 4928x3264+0+0
-    # Resolution: 300x300
-    # Print size: 16.4267x10.88
-    MODEL=$(identify -verbose "${FILE}" | grep "exif:Model:")
-    SIGNATURE=$(identify -verbose "${FILE}" | grep "signature:")
-    GEOMETRY=$(identify -verbose "${FILE}" | grep "Geometry:")
-    RESOLUTION=$(identify -verbose "${FILE}" | grep "^  Resolution:")
-    IMG_SIZE=$(identify -verbose "${FILE}" | grep "Print size:")
+    PIC_SIZE=$(identify -format "%wx%h\n" "${FILE}")
+    #echo "PIC_SIZE: ${PIC_SIZE}"
 
-    MODEL=$(echo "${MODEL}" | awk -F": " '{ print $2 }')
-    SIGNATURE=$(echo "${SIGNATURE}" | awk -F": " '{ print $2 }')
-    GEOMETRY=$(echo "${GEOMETRY}" | awk -F": " '{ print $2 }')
-    RESOLUTION=$(echo "${RESOLUTION}" | awk -F": " '{ print $2 }')
-    IMG_SIZE=$(echo "${IMG_SIZE}" | awk -F": " '{ print $2 }')
-
-    # echo "MODEL      : ${MODEL}"
-    # echo "SIGNATURE  : ${SIGNATURE}"
-    # echo "GEOMETRY   : ${GEOMETRY}"
-    # echo "RESOLUTION : ${RESOLUTION}"
-    # echo "IMG_SIZE   : ${IMG_SIZE}"
-
-    echo "${FILENAME} # ${ACTUAL_FILE_SIZE} # ${SIGNATURE} # ${GEOMETRY} # ${RESOLUTION} # ${IMG_SIZE} # ${MODEL} # ${FILE}" | tee -a ${FILE_INFO_LIST}
+    echo "${FILENAME} # ${ACTUAL_FILE_SIZE} # ${PIC_SIZE} # ${FILE}" | tee -a ${FILE_INFO_LIST}
 done < ${FILE_LIST}
-
-
 
 popd > /dev/null
