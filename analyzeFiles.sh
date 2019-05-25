65569e6b425c33443b78cfbfdb965372f905302f
Reto <reto271@gmx.ch>
2019-05-25 22:38:51 +0200

    Prepare Analyze Script
    Based on this results directories with copies of images are identified. Take action to cleanup. Afterwards run the findDoubles.sh followed by analyze.sh again.

---
+++ analyzeFiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change to the script dir
+pushd . > /dev/null
+ScriptDirectory=$(dirname $0)
+cd "$ScriptDirectory"/out_dir
+
+cat fileInfoList.txt | sort -n > fileList01.txt
+cat fileList01.txt | grep -v "/volume1/zuniPics/all/" > fileList02.txt
+
+cat fileList02.txt | grep "SYNOPHOTO_THUMB_PREVIEW.jpg" > fileList03_syno1.txt
+cat fileList02.txt | grep "SYNOPHOTO_THUMB_S.jpg" > fileList03_syno2.txt
+cat fileList02.txt | grep "SYNOPHOTO:THUMB_S.jpg" > fileList03_syno3.txt
+cat fileList02.txt | \
+    grep -v "SYNOPHOTO_THUMB_PREVIEW.jpg" | \
+    grep -v "SYNOPHOTO_THUMB_S.jpg" | \
+    grep -v "SYNOPHOTO:THUMB_S.jpg" > fileList03_noSyno.txt
+
+# fileList04 manually deleted some pics
+
+cat fileList04_manual.txt | grep -v "@eaDir" > fileList05.txt
+
+#
+
+# Return to the original directory
+popd > /dev/null
