878c8a5c92f6d889079a394860a2f4bcd15afea4
Reto <reto271@gmx.ch>
2020-04-14 08:53:46 +0200

    Improve analysis

--- analyzeFiles.sh
+++ analyzeFiles.sh
@@ -5,22 +5,33 @@ pushd . > /dev/null
 ScriptDirectory=$(dirname $0)
 cd "$ScriptDirectory"/out_dir
 
-cat fileInfoList.txt | sort -n > fileList01.txt
-cat fileList01.txt | grep -v "/volume1/zuniPics/all/" > fileList02.txt
+cat fileInfoList.txt | sort -n > fileInfoList01.txt
 
-cat fileList02.txt | grep "SYNOPHOTO_THUMB_PREVIEW.jpg" > fileList03_syno1.txt
-cat fileList02.txt | grep "SYNOPHOTO_THUMB_S.jpg" > fileList03_syno2.txt
-cat fileList02.txt | grep "SYNOPHOTO:THUMB_S.jpg" > fileList03_syno3.txt
-cat fileList02.txt | \
-    grep -v "SYNOPHOTO_THUMB_PREVIEW.jpg" | \
-    grep -v "SYNOPHOTO_THUMB_S.jpg" | \
-    grep -v "SYNOPHOTO:THUMB_S.jpg" > fileList03_noSyno.txt
+# Remove unused
+cat fileInfoList01.txt | grep -v "@eaDir" > fileInfoList02.txt
+cat fileInfoList02.txt | grep -v "/volume1/photo/all/" > fileInfoList03.txt
 
-# fileList04 manually deleted some pics
-# cat fileList04_manual.txt | grep -v "@eaDir" > fileList05.txt
-cat fileList03_noSyno.txt | grep -v "@eaDir" > fileList05.txt
 
-#
+# Finalize - find doubles
+cp fileInfoList03.txt fileInfoFinal01.txt
+
+rm fileInfoFinal02.txt
+touch fileInfoFinal02.txt
+
+while read line ; do
+    #SIZE=$(cat ${line} | awk '{ print $1 }')
+
+#      if ( ${SIZE} -eq ${SIZE_OLD} ) ; then
+    echo "${line}" >> fileInfoFinal02.txt
+#          echo "${SIZE}" >> fileInfoFinal02.txt
+#          echo "${LINE_OLD}" >> fileInfoFinal02.txt
+#          echo "---" >> fileInfoFinal02.txt
+#      fi
+#      LINE_OLD=${LINE}
+#      SIZE_OLD=${SIZE}
+
+done < fileInfoFinal01.txt
+
 
 # Return to the original directory
 popd > /dev/null
