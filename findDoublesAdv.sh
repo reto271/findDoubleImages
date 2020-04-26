3a6b174dc54cce9ecc3b8b2db12dc818d489ad35
Reto <reto271@gmx.ch>
2020-04-26 15:57:16 +0200

    Prepare to use on NAS

--- findDoublesAdv.sh
+++ findDoublesAdv.sh
@@ -8,12 +8,12 @@
 #--------------------------------------------------------
 
 # SEARCH_PATH default definion
-#SEARCH_PATH=/home/reto/Pictures
-#SEARCH_PATH=/home/reto/Pictures/2012/20120512_SpielenStattEssen
-SEARCH_PATH=sampleImg
+#SEARCH_PATH=/volume1/photo
+SEARCH_PATH=/volume1
+#SEARCH_PATH=sampleImg
+
 OUT_DIR=out_dir
 FILE_LIST=${OUT_DIR}/fileList.txt
-FILE_INFO_LIST=${OUT_DIR}/fileInfoList.txt
 
 pushd . > /dev/null
 ScriptDirectory=$(dirname $0)
@@ -33,7 +33,7 @@ find ${SEARCH_PATH} -iname "*.jpeg" > ${FILE_LIST}
 find ${SEARCH_PATH} -iname "*.jpg" >> ${FILE_LIST}
 
 echo "--- Remove pics out of scope, e.g. e-mail box"
-cat "${FILE_LIST}" | grep -v "/volume1/Reto/Mail/V2/" | grep -v "/volume1/zbkMac_01/mailBackup" > ${FILE_LIST}.2
+cat "${FILE_LIST}" | grep -v "/volume1/Reto/Mail/V2/" | grep -v "@eaDir" > ${FILE_LIST}.2
 mv ${FILE_LIST}.2 ${FILE_LIST}
 
 
